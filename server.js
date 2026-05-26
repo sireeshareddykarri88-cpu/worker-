99c829b1719a1f7374d3dc0504364be4769acf79
sireeshareddykarri88-cpu <sireeshareddykarri88@gmail.com>
2026-05-26 13:38:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+
 const cors = require("cors");
+
 const dotenv = require("dotenv");
-const { createClient } = require("@supabase/supabase-js");
+
+const { createClient }
+  = require("@supabase/supabase-js");
+
 const path = require("path");
 
 dotenv.config();
@@ -12,22 +17,14 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static(__dirname));
-
-if (
-  !process.env.SUPABASE_URL ||
-  !process.env.SUPABASE_SERVICE_KEY
-) {
-
-  console.log("ENV FILE ERROR");
-
-  process.exit(1);
-
-}
+app.use(express.static("public"));
 
 const supabase = createClient(
+
   process.env.SUPABASE_URL,
+
   process.env.SUPABASE_SERVICE_KEY
+
 );
 
 app.post("/register", async (req, res) => {
@@ -42,7 +39,8 @@ app.post("/register", async (req, res) => {
       qualification
     } = req.body;
 
-    const { data, error } = await supabase
+    const { data, error }
+      = await supabase
       .from("workers")
       .insert([
         {
@@ -65,7 +63,6 @@ app.post("/register", async (req, res) => {
 
     res.json({
       success: true,
-      message: "Successfully Registered",
       data
     });
 
@@ -80,10 +77,13 @@ app.post("/register", async (req, res) => {
 
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT =
+  process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on ${PORT}`
+  );
 
 });
